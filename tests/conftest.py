dda444fc01aecbb58387a2372658e5b5f20974fe
Choi Bochul <vio.bo94@gmail.com>
2014-12-03 21:33:49 +0900

    Move local dyanodb libraray to tests package

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,17 +11,17 @@ process = None
 
 def pytest_configure():
     global process
-    shutil.rmtree('dynamodb/testdb', True)
-    os.mkdir('dynamodb/testdb')
+    shutil.rmtree('tests/local_dynamodb/testdb', True)
+    os.mkdir('tests/local_dynamodb/testdb')
 
     host = 'localhost'
     port = 8001
     dev_null = open(os.devnull, 'wb')
     process = subprocess.Popen([
         '/usr/bin/java', '-Djava.net.preferIPv4Stack=true',
-        '-Djava.library.path=./dynamodb/DynamoDBLocal_lib', '-jar',
-        'dynamodb/DynamoDBLocal.jar', '-port', str(port),
-        '-dbPath', './dynamodb/testdb'
+        '-Djava.library.path=tests/local_dynamodb/DynamoDBLocal_lib', '-jar',
+        'tests/local_dynamodb/DynamoDBLocal.jar', '-port', str(port),
+        '-dbPath', 'tests/local_dynamodb/testdb/'
     ], stdout=dev_null, stderr=dev_null)
 
     patch_dynamodb_connection(host=host, port=port, is_secure=False)
