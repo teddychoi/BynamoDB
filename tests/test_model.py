f2cd786713593a44a802a5b57ac72be208011cf5
Choi Bochul <vio.bo94@gmail.com>
2014-12-05 16:51:39 +0900

    Coding convention

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,7 +8,7 @@ from bynamodb.model import Attribute, Model, GlobalAllIndex, AllIndex
 
 
 @fixture
-def test_model():
+def fx_test_model():
     class TestModel(Model):
         hash_key_attr = Attribute(STRING, hash_key=True)
         range_key_attr = Attribute(STRING, range_key=True)
@@ -16,21 +16,21 @@ def test_model():
     return TestModel
 
 
-def test_table_attr_name(test_model):
-    assert test_model.hash_key_attr.attr_name == 'hash_key_attr'
-    assert test_model.range_key_attr.attr_name == 'range_key_attr'
-    assert test_model.attr_1.attr_name == 'attr_1'
+def test_table_attr_name(fx_test_model):
+    assert fx_test_model.hash_key_attr.attr_name == 'hash_key_attr'
+    assert fx_test_model.range_key_attr.attr_name == 'range_key_attr'
+    assert fx_test_model.attr_1.attr_name == 'attr_1'
 
 
-def test_create_table(test_model):
-    test_model.create_table()
-    table_description = DynamoDBConnection().describe_table(test_model.get_table_name())[u'Table']
+def test_create_table(fx_test_model):
+    fx_test_model.create_table()
+    table_description = DynamoDBConnection().describe_table(fx_test_model.get_table_name())['Table']
     expected_key_name = {
         'HASH': 'hash_key_attr',
         'RANGE': 'range_key_attr'
     }
-    for key in table_description[u'KeySchema']:
-        assert expected_key_name[key[u'KeyType']] == key[u'AttributeName']
+    for key in table_description['KeySchema']:
+        assert expected_key_name[key['KeyType']] == key['AttributeName']
 
 
 @fixture
@@ -66,24 +66,6 @@ def test_create_table_with_global_index(fx_table_with_global_index):
         assert expected_key_name[key['KeyType']] == key['AttributeName']
 
 
-def test_get_item(test_model):
-    test_model.create_table()
-
-    hash_key_value = 'Hash Key Value'
-    range_key_value = 'Range Key Value'
-    attr1_value = 'Attribute1 Value'
-    test_model.put_item({
-        'hash_key_attr': hash_key_value,
-        'range_key_attr': range_key_value,
-        'attr_1': attr1_value
-    })
-
-    item = test_model.get_item(hash_key_value, range_key_value)
-    assert item.hash_key_attr == hash_key_value
-    assert item.range_key_attr == range_key_value
-    assert item.attr_1 == attr1_value
-
-
 @fixture
 def fx_table_with_local_index():
     class TestModel(Model):
@@ -115,8 +97,26 @@ def test_create_table_with_local_index(fx_table_with_local_index):
         assert expected_key_name[key['KeyType']] == key['AttributeName']
 
 
-def test_put_item(test_model):
-    test_model.create_table()
+def test_get_item(fx_test_model):
+    fx_test_model.create_table()
+
+    hash_key_value = 'Hash Key Value'
+    range_key_value = 'Range Key Value'
+    attr1_value = 'Attribute1 Value'
+    fx_test_model.put_item({
+        'hash_key_attr': hash_key_value,
+        'range_key_attr': range_key_value,
+        'attr_1': attr1_value
+    })
+
+    item = fx_test_model.get_item(hash_key_value, range_key_value)
+    assert item.hash_key_attr == hash_key_value
+    assert item.range_key_attr == range_key_value
+    assert item.attr_1 == attr1_value
+
+
+def test_put_item(fx_test_model):
+    fx_test_model.create_table()
     hash_key_value = 'Hash Key Value'
     range_key_value = 'Range Key Value'
     attr1_value = 'Attribute1 Value'
@@ -125,27 +125,27 @@ def test_put_item(test_model):
         'range_key_attr': range_key_value,
         'attr_1': attr1_value
     }
-    test_model.put_item(attrs)
+    fx_test_model.put_item(attrs)
 
-    item = test_model.get_item(hash_key=hash_key_value, range_key=range_key_value)
+    item = fx_test_model.get_item(hash_key=hash_key_value, range_key=range_key_value)
     assert item.hash_key_attr == hash_key_value
     assert item.range_key_attr == range_key_value
     assert item.attr_1 == attr1_value
 
 
-def test_put_item_with_missing_attr(test_model):
-    test_model.create_table()
+def test_put_item_with_missing_attr(fx_test_model):
+    fx_test_model.create_table()
     attrs = {
         'hash_key_attr': 'hash_key',
         'range_key_attr': 'range_key'
     }
     with raises(NullAttributeException):
-        test_model.put_item(attrs)
+        fx_test_model.put_item(attrs)
 
 
-def test_save_item_with_missing_attr(test_model):
-    test_model.create_table()
-    item = test_model({
+def test_save_item_with_missing_attr(fx_test_model):
+    fx_test_model.create_table()
+    item = fx_test_model({
         'hash_key_attr': 'hash_key',
         'range_key_attr': 'range_key'
     })
@@ -154,20 +154,20 @@ def test_save_item_with_missing_attr(test_model):
 
 
 @fixture
-def model_with_nullable_attr():
+def fx_model_with_nullable_attr():
     class TestModelWithNullable(Model):
         hash_key = Attribute(STRING, hash_key=True)
         attr = Attribute(STRING, null=True)
     return TestModelWithNullable
 
 
-def test_save_item_nullable_attr_emptied(model_with_nullable_attr):
-    model_with_nullable_attr.create_table()
+def test_save_item_nullable_attr_emptied(fx_model_with_nullable_attr):
+    fx_model_with_nullable_attr.create_table()
     hash_key_value = 'value'
-    model_with_nullable_attr({
+    fx_model_with_nullable_attr({
         'hash_key': hash_key_value
     }).save()
-    item = model_with_nullable_attr.get_item(hash_key_value)
+    item = fx_model_with_nullable_attr.get_item(hash_key_value)
     assert item.hash_key == hash_key_value
 
 
